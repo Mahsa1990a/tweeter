410860455f60a10551d2f22e1cea7a0140271c74
Mahsa Arabameri <amerimahsa@yahoo.com>
2020-12-15 23:12:21 +0000

    updated

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -9,19 +9,20 @@ $(document).ready(function() {
   //console.log("Hii");
   //console.log(document);
   //console.log($(document).ready)
-  $("textarea").on("keyup", function() {
-    //console.log("This: ", this); //this -> <textarea ...></textarea>
-
-    const count = $(this).val().length;
-    console.log("count", count); //it increases number of characters that I type(start from 0 so we need 140-count)
-    const counter = 140 - count;
-    //target is output that has 140 counter:
-    $("output").text(counter); //it shows decreasing 140
-
-    if (counter < 0) {
-      $("output").addClass("counterErr");
-    } else {
-      $("output").removeClass("counterErr");
-    }
-  });
+  $("textarea").on("keyup", onKeyUp);
 });
+
+const onKeyUp = function() {
+    //console.log("This: ", this); //this -> <textarea ...></textarea>
+  const count = $(this).val().length;
+  console.log("count", count); //it increases number of characters that I type(start from 0 so we need 140-count)
+  const counter = 140 - count;
+  //target is output that has 140 counter:
+  $("output").text(counter); //it shows decreasing 140
+
+  if (counter < 0) {
+    $("output").addClass("counterErr");
+  } else {
+    $("output").removeClass("counterErr");
+  }
+};

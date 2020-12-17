4e42fafd49764da218dec1150998fba24eb0d71f
Mahsa Arabameri <amerimahsa@yahoo.com>
2020-12-17 22:13:43 +0000

    cleaning code

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,28 +2,26 @@
 
 //alert("helloooo!"); for testing if my js file is working well
 
-//Add this to file to ensure the DOM has loaded
 //$(document).ready runs a callback when the DOM is ready to be manipulated with jQuery
 $(document).ready(function() {
-  
-  //console.log("Hii");
-  //console.log(document);
-  //console.log($(document).ready)
+
   $("textarea").on("keyup", onKeyUp);
+
 });
 
 const onKeyUp = function() {
-    //console.log("This: ", this); //this -> <textarea ...></textarea>
+  //console.log("This: ", this); //this -> <textarea ...></textarea>
   const count = $(this).val().length;
   //console.log("count", count); //it increases number of characters that I type(start from 0 so we need 140-count)
   const counter = 140 - count;
-  //target is output that has 140 counter:
-  $("output").text(counter); //it shows decreasing 140
-  //or $(this).siblings().children(".counter").text(counter);
+
+  $(this).siblings().children(".counter").text(counter);
+  //or  $("output").text(counter); //it shows decreasing 140
+  
 
   if (counter < 0) {
-    $("output").addClass("counterErr");
+    $(this).siblings().children(".counter").addClass("counterErr");
   } else {
-    $("output").removeClass("counterErr");
+    $(this).siblings().children(".counter").removeClass("counterErr");
   }
 };
